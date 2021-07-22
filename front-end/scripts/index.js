11106a32fd86da3b15e6f5385c2ae0bdc19d29cb
Gabriel <kahasocihal@gmail.com>
2021-07-22 02:20:25 +0200

    index html + js

--- front-end/scripts/index.js
+++ front-end/scripts/index.js
@@ -17,5 +17,47 @@ function getArticles() {
       productsContainer.style.padding = "30vh 0";
     })
 
+.then(function (resultatAPI) {
+    const articles = resultatAPI;
+    console.log(articles);
+    for (let article in articles) {
+      let productCard = document.createElement("div");
+      document.querySelector(".products").appendChild(productCard);
+      productCard.classList.add("product");
+
+      let productLink = document.createElement("a");
+      productCard.appendChild(productLink);
+      productLink.href = `product.html?id=${resultatAPI[article]._id}`;
+      productLink.classList.add("stretched-link");
+
+      let productImgDiv = document.createElement("div");
+      productLink.appendChild(productImgDiv);
+      productImgDiv.classList.add("product__img");
+
+      let productImg = document.createElement("img");
+      productImgDiv.appendChild(productImg);
+      productImg.src = resultatAPI[article].imageUrl;
+
+      let productInfosDiv = document.createElement("div");
+      productLink.appendChild(productInfosDiv);
+      productInfosDiv.classList.add("product__infos");
+
+      let productInfoTitle = document.createElement("div");
+      productInfosDiv.appendChild(productInfoTitle);
+      productInfoTitle.classList.add("product__infos__title");
+      productInfoTitle.innerHTML = resultatAPI[article].name;
+
+      let productInfoPrice = document.createElement("div");
+      productInfosDiv.appendChild(productInfoPrice);
+      productInfoPrice.classList.add("product__infos__price");
+
+     
+      resultatAPI[article].price = resultatAPI[article].price / 100;
+      productInfoPrice.innerHTML = new Intl.NumberFormat("fr-FR", {
+        style: "currency",
+        currency: "EUR",
+      }).format(resultatAPI[article].price);
+    }
+  });
     
 }
